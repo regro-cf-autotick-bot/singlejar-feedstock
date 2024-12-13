808f0d70ba02a18b9b21c7be345302eda059a5ff
Uwe L. Korn <uwe.korn@quantco.com>
2024-12-13 14:48:09 +0100

    Update patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,6 @@ source gen-bazel-toolchain
 export PROTOC_VERSION=$(conda list -p $PREFIX libprotobuf | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ' | sed -E 's/^[0-9]+\.([0-9]+\.[0-9]+)$/\1/')
 export PROTOBUF_JAVA_MAJOR_VERSION="3"
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk"
-sed -ie "s:PROTOC_VERSION:${PROTOC_VERSION}:" WORKSPACE
-sed -ie "s:PROTOBUF_JAVA_MAJOR_VERSION:${PROTOBUF_JAVA_MAJOR_VERSION}:" WORKSPACE
 sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL:-install_name_tool}:" src/BUILD
 sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
